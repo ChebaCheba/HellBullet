09c98c5fe250ee43004436e35e466a7982c98787
ChebaCheba <bastiangaxiola@gmail.com>
2020-07-10 00:28:44 -0500

    Ship Movement
    Ship now moves and faces mouse

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour
+{
+    private float h, v;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveShip();
+        FaceMouse();
+    }
+
+    void MoveShip(){
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+        transform.Translate(h * Time.deltaTime * 6, 0, 0, Space.World);
+        transform.Translate(0, v * Time.deltaTime * 6, 0, Space.World);
+    }
+
+    void FaceMouse()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        Vector2 direction = new Vector2(
+                mousePos.x - transform.position.x,
+                mousePos.y - transform.position.y
+        );
+        transform.up = direction;
+    }
+}
